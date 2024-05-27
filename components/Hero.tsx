57465e98c278d19f627d6ed864fa43eb13c51dc7
pocketcorp <pocketcorp@protonmail.com>
2024-05-27 09:48:17 -0700

    hero section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import { Spotlight } from './ui/Spotlight'
+import { TextGenerateEffect } from './TextGenerateEffect'
+import MagicButton from './MagicButton'
+import { FaLocationArrow } from "react-icons/fa6";
 
 
 const Hero = () => {
@@ -15,9 +18,24 @@ const Hero = () => {
         </div>
         <div className="flex justify-center relative my-20 z-10">
           <div className="max-w-[89] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
-            <div className="uppercase tracking-widest text-xs text-center text-blue-100 max-x-80">
+            <h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-x-80">
               Dynamic Wab Magic with Next.js
-            </div>
+            </h2>
+
+            <TextGenerateEffect 
+              className='text-center text-[40px] md:text5xl lg:6xl'
+              words='Transforming Concepts into Seamless Experiences'
+            />
+            <p className='text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl'>
+              Hi, this is PocketCorp, a Next.js Developer
+            </p>
+            <a href="#about">
+              <MagicButton 
+                title='Show my work'
+                icon={<FaLocationArrow />}
+                position='right'
+              />
+            </a>
           </div>
         </div>
     </div>
